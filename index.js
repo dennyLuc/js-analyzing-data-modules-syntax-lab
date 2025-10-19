f0b1756c25aae3ec0b2a289fe336aa1b18379ae5
Deniz Lucas <deniz4376@gmail.com>
2025-10-19 11:26:48 +0300

    My first attempt at Rest and Spread in JS

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+  };
 
+  args.forEach(array => {
+    combinedObject.users = [...combinedObject.users, ...array];
+  });
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
+
+// Example usage:
+const result = combineUsers(['user1', 'user2'], ['user3', 'user4']);
+console.log(result);
+// Output: { users: ['user1', 'user2', 'user3', 'user4'], merge_date: '10/1/2023' }
